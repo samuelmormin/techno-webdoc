9e32c7515ca846a596383a0db925b1b96b72dd6e
Robsiii <robin.chateau@hetic.net>
2017-06-29 14:59:53 +0200

    :lipstuck: page 10 a 19

---
+++ src/components/page14.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-full">
+    <div class="video">
+      <video class="video-background1" autoplay="autoplay" loop="loop">
+        <source src="/static/music-berlin.mp4"  type="video/mp4"  />
+        Your browser doesn't support video API
+      </video>
+      <h4>UNE VISION COMMUNE</h4>
+      <p>Les deux parties de la ville sont rythmées par ce nouveau style musical grandissant. Les gens se rejoignent dans la musique malgré quelques différences de vie. Les inégalités s’estompent face à la musique.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'chapter4'
+  }
+</script>
